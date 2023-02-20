b25b0db3fbb9a9c08d8a2638b08443251c2983d6
ana <ana@braip.comgit config --global user.email ana@braip.comgit config --global user.email ana@braip.com>
2023-02-20 15:13:56 -0300

    Acrescendo o arquivo "test_fav_order.py", teste automatizado, filtro ordenação na tela favoritos

---
+++ test_fav_order.py
@@ -0,0 +1,47 @@
+from selenium.webdriver.common.by import By
+import time
+from setup_login import driver, braip_maps
+import unittest
+from test_login import LoginTest
+
+
+class FavOrderTest(unittest.TestCase):
+
+
+    def test_login_bem_sucedido_fav_order(self):
+        max_attempts = 5
+        attempts = 0
+
+        while attempts < max_attempts:
+            LoginTest            
+            time.sleep(2)
+            field_favorites = driver.find_element(By.XPATH,braip_maps["buttons"]["send_favorites"]["xpath"])
+            field_favorites.click()
+            time.sleep(1)
+            field_order = driver.find_element(By.XPATH,braip_maps["buttons"]["selection"]["xpath"])
+            field_order.click()
+            time.sleep(1)
+            field_order = driver.find_element(By.XPATH,braip_maps["buttons"]["antigos"]["xpath"])
+            field_order.click()
+            time.sleep(1)
+            field_order = driver.find_element(By.XPATH,braip_maps["buttons"]["antigos"]["xpath"])
+            field_order.click()
+            time.sleep(1)
+            field_order = driver.find_element(By.XPATH,braip_maps["buttons"]["selection"]["xpath"])
+            field_order.click()
+            time.sleep(1)
+
+            try:
+                title = driver.title
+                assert 'Braip Members - Favoritos' in title
+                print(title)
+                driver.save_screenshot('screen_07.png')
+                home = driver.find_element(By.XPATH,braip_maps["buttons"]["home"]["xpath"])
+                home.click()
+                time.sleep(1)
+                break
+            except:
+                attempts += 1
+                print(title)
+                if attempts == max_attempts:
+                    raise
