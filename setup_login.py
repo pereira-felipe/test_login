7fbfafbabfbf1fe40005f1c4357e9a2d3d718adf
ana <ana@braip.comgit config --global user.email ana@braip.comgit config --global user.email ana@braip.com>
2023-02-20 14:57:17 -0300

    refatoração e melhoria do código

--- setup_login.py
+++ setup_login.py
@@ -25,10 +25,61 @@ braip_maps = {
             "xpath": "/html/body/div[1]/div/div/main/div/div[2]/form/div[1]/div[1]/div[2]/div/div/input"
         },
         "erro_email": {
-            "xpath": "/html/body/div[1]/div/div/main/div/div[2]/form/div[1]/div[1]/div[1]/div[2]/ul/li"
+            "xpath": "/html/body/div[1]/div/div/main/div/div[2]/form/div[1]/div[1]/div[1]/div[2]"
         },
         "erro_senha": {
             "xpath": "/html/body/div[1]/div/div/main/div/div[2]/form/div[1]/div[1]/div[2]/div[2]/ul/li"
+        },
+        "send_like": {
+            "xpath": "/html/body/div[1]/div/div/main/div[2]/section/div[2]/div/section/div[2]/div/div[2]/span/div[1]"
+        },
+        "send_search": {
+            "xpath": "/html/body/div[1]/div/div/main/div[2]/section/div[1]/div/div/ul/li[1]/div/div"
+        },
+        "send_search_key": {
+            "xpath": "/html/body/div[1]/div/div/main/div[2]/section/div[1]/div/div/ul/li[1]/div/div[1]/input"
+        },
+        "send_favorites": {
+            "xpath": "/html/body/div[1]/div/div/main/div[2]/section/div[1]/div/div/ul/li[2]/a/div"
+        },
+        "home": {
+            "xpath": "/html/body/div[1]/div/div/main/section/div[1]/div/div/div/a/div/img"
+        },
+        "notification": {
+            "xpath": "/html/body/div[1]/div/div/main/div[2]/section/div[1]/div/div/ul/li[3]/div/button/div/div"
+        },
+        "selection": {
+            "xpath": "/html/body/div[1]/div/div/main/section/div[2]/div/div/div[1]/div[1]/div/div/details/summary"
+        },
+        "antigos": {
+            "xpath": "/html/body/div[1]/div/div/main/section/div[2]/div/div/div[1]/div[1]/div/div/details/ul/li[2]/label"
+        },
+        "recentes": {
+            "xpath": "/html/body/div[1]/div/div/main/section/div[2]/div/div/div[1]/div[1]/div/div/details/ul/li[1]/label"
+        },
+        "aula": {
+            "xpath": "/html/body/div[1]/div/div/main/div[2]/section/div[2]/div/div/div[1]/section/div[2]/div[1]/div/div/div[1]"
+        },
+        "perfil": {
+            "xpath": "/html/body/div[1]/div/div/main/div[2]/section/div[1]/div/div/ul/li[4]/div/button/div/div[1]/img"
+        },
+        "logout": {
+            "xpath": "/html/body/div[1]/div/div/main/div[2]/section/div[1]/div/div/ul/li[4]/div/div/div[2]/div/div[6]/div"
+        },
+        "profile_account": {
+            "xpath": "/html/body/div[1]/div/div/main/div[2]/section/div[1]/div/div/ul/li[4]/div/div/div[2]/div/div[1]/div[1]"
+        },
+        "progress": {
+            "xpath": "/html/body/div[1]/div/div/main/div[2]/section/div[2]/div/div/div[2]/section/div[1]"
+        },
+        "account": {
+            "xpath": "/html/body/div[1]/div/div/main/div[2]/section/div[1]/div/div/ul/li[4]/div/div/div[2]/div/div[2]/div[1]"
+        },
+        "close": {
+            "xpath": "/html/body/div[1]/div/div/main/div[2]/section/div[1]/div/div/ul/li[4]/div/div/div[1]/div[2]/button/div/i"
+        },
+        "support": {
+            "xpath": "/html/body/div[1]/div/div/main/div[2]/section/div[1]/div/div/ul/li[4]/div/div/div[2]/div/div[4]/div[1]"
         }
     }
 }
